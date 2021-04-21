808ec8d29dfdbc3753c95dcc2da22ae1f475dd0e
Arthur Lemaire <arthur.lemaire@epitech.eu>
2021-04-21 15:39:34 +0200

    tentative de fix serviceWorker registration

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -34,7 +34,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `./service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         console.log("is localhost")
